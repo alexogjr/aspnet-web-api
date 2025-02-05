7d9e4ef6e624cafec74343366114dd3459a7f944
unknown <alex.sandro.junior.dev@gmail.com>
2025-02-05 20:56:00 -0300

    melhorando o projeto

--- Projeto/Controllers/TarefaListController.cs
+++ Projeto/Controllers/TarefaListController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Projeto.Models;
+using Projeto.Data;
 
 namespace Projeto.Controllers
 {
@@ -9,11 +10,14 @@ namespace Projeto.Controllers
     public class TarefaListController : ControllerBase
     {
 
-        private static List<Models.tarefaList> _tarefas = new()
-        {
-            new Models.tarefaList { TarefaId = 1, NomeTarefa = "Fazer compras", DescricaoTarefa = "Macarrão", Date = DateOnly.FromDateTime(DateTime.Now) },
-            new Models.tarefaList { TarefaId = 2, NomeTarefa = "Estudar C#", DescricaoTarefa = "WebAPI", Date = DateOnly.FromDateTime(DateTime.Now.AddDays(1)) }
-        };
+        //private static List<Models.tarefaList> _tarefas = new()
+        //{
+        //    new Models.tarefaList { TarefaId = 1, NomeTarefa = "Fazer compras", DescricaoTarefa = "Macarrão", Date = DateOnly.FromDateTime(DateTime.Now) },
+        //    new Models.tarefaList { TarefaId = 2, NomeTarefa = "Estudar C#", DescricaoTarefa = "WebAPI", Date = DateOnly.FromDateTime(DateTime.Now.AddDays(1)) }
+        //};
+
+        private static List<Models.tarefaList> _tarefas = TarefaData._tarefas; // Correção aqui
+
 
         // busca todas as tarefas
         [HttpGet]
---
+++ Projeto/Data/TarefaData.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Projeto.Data
+{
+    public class TarefaData
+    {
+
+        public static readonly List<Projeto.Models.tarefaList> _tarefas = new()
+        {
+            new Projeto.Models.tarefaList { TarefaId = 1, NomeTarefa = "Fazer compras", DescricaoTarefa = "Macarrão", Date = DateOnly.FromDateTime(DateTime.Now) },
+            new Projeto.Models.tarefaList { TarefaId = 2, NomeTarefa = "Estudar C#", DescricaoTarefa = "WebAPI", Date = DateOnly.FromDateTime(DateTime.Now.AddDays(1)) }
+        };
+
+    }
+}
